6657352488f640111c23b689134ad1417f88cea0
Emmanuel-Baraka <emmanuelbaraka0507@gmail.com>
2025-07-03 16:10:12 +0300

    Create Do while example.java

---
+++ Do while example.java
@@ -0,0 +1,10 @@
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int number = 0;
+
+        do {
+            System.out.println(number);
+            number++;
+        } while (number <= 40);
+    }
+}
